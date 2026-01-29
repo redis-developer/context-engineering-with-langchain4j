7414b42a89d35b7b49ae7d4864512efed5985c80
Ricardo Ferreira <riferrei@riferrei.com>
2026-01-29 10:42:27 -0500

    Initial commit

--- backend-layer/src/main/java/io/redis/devrel/workshop/memory/LongTermMemory.java
+++ backend-layer/src/main/java/io/redis/devrel/workshop/memory/LongTermMemory.java
@@ -2,21 +2,14 @@ package io.redis.devrel.workshop.memory;
 
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.input.PromptTemplate;
-import dev.langchain4j.model.scoring.ScoringModel;
-import dev.langchain4j.model.scoring.onnx.OnnxScoringModel;
 import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.Content;
-import dev.langchain4j.rag.content.aggregator.ContentAggregator;
-import dev.langchain4j.rag.content.aggregator.ReRankingContentAggregator;
 import dev.langchain4j.rag.content.injector.ContentInjector;
 import dev.langchain4j.rag.content.injector.DefaultContentInjector;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.query.router.LanguageModelQueryRouter;
 import dev.langchain4j.rag.query.router.QueryRouter;
-import dev.langchain4j.rag.query.transformer.CompressingQueryTransformer;
-import dev.langchain4j.rag.query.transformer.QueryTransformer;
-import io.redis.devrel.workshop.config.ModelManager;
 import io.redis.devrel.workshop.services.MemoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -33,13 +26,8 @@ public class LongTermMemory {
     @Autowired
     private MemoryService memoryService;
 
-    @Autowired
-    private ModelManager modelManager;
-
     @Bean
     public RetrievalAugmentor getRetrievalAugmentor(ChatModel chatModel) {
-        QueryTransformer queryTransformer = new CompressingQueryTransformer(chatModel);
-
         ContentInjector contentInjector = DefaultContentInjector.builder()
                 .promptTemplate(PromptTemplate.from("{{userMessage}}\n\n[Context]\n{{contents}}"))
                 .build();
@@ -54,20 +42,9 @@ public class LongTermMemory {
                 .fallbackStrategy(LanguageModelQueryRouter.FallbackStrategy.ROUTE_TO_ALL)
                 .build();
 
-        ScoringModel scoringModel = new OnnxScoringModel(
-                modelManager.getModelPath(),
-                modelManager.getTokenizerPath()
-        );
-        ContentAggregator contentAggregator = ReRankingContentAggregator.builder()
-                .scoringModel(scoringModel)
-                .minScore(0.8)
-                .build();
-
         return DefaultRetrievalAugmentor.builder()
-                .queryTransformer(queryTransformer)
                 .contentInjector(contentInjector)
                 .queryRouter(queryRouter)
-                .contentAggregator(contentAggregator)
                 .build();
     }
 
